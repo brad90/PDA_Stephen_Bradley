7df6541142e9a46b70ed671bf9d7d7a918949a04
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-05-05 16:57:59 +0100

    test of the card class completed

---
+++ PDA_Static_and_Dynamic_Task_A/task_2/specs/card_specs.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../card.rb')
+
+
+
+class Test_card < MiniTest::Test
+
+  def setup
+    @card1 = Card.new("heart", 3)
+  end
+
+  def test_card_suit
+    assert_equal("heart", @card1.suit)
+  end
+
+  def test_card_value
+    assert_equal(3, @card1.value)
+  end
+
+
+end
