2bd9392e7f4adec92e732f770f6210c38cba2f97
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-25 09:56:48 +0100

    js testing added

---
+++ js_calculator_start_test/tests/integration/tests.js
@@ -0,0 +1,69 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should have display of the opereration', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  it('should have multiple operatortions', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+
+  })
+
+  it('should have divide by 0 operatortions', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+  })
+
+
+  it('should have negative numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('-1')
+
+//
+// element(by.css('#clear')).click();
+
+  })
+
+
+});
